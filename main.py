e0871d1eafd2b41331bd68bfed1f1beb9e1305fb
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-06 16:05:34 +0100

    Variables and Data types

--- main.py
+++ main.py
@@ -1,1 +1,119 @@
-import pandas as pd
+# Imported Functions from Packages
+import random
+
+# Rendering to Output
+print("Hello, World!") #This is a comment
+
+# Python Indentation
+if 5 > 2:
+    print("Five is greater than two!")
+
+# variables are created when you assign a value to it
+x = 5
+y = "John"
+print(x)
+print(y)
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+# Casting
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+# Global Variables
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+myfunc()
+
+def myfunc():
+  global x
+  x = "fantastic"
+myfunc()
+
+print("Python is " + x)
+
+#Data Types
+x = "Hello World"	#str	
+x = 20	#int	
+x = 20.5	#float	
+x = 1j	#complex	
+x = ["apple", "banana", "cherry"]	#list	
+x = ("apple", "banana", "cherry")	#tuple	
+x = range(6)	#range	
+x = {"name" : "John", "age" : 36}	#dict	
+x = {"apple", "banana", "cherry"}	#set	
+x = frozenset({"apple", "banana", "cherry"})	#frozenset	
+x = True	#bool	
+x = b"Hello"	#bytes	
+x = bytearray(5)	#bytearray	
+x = memoryview(bytes(5))	#memoryview	
+x = None	#NoneType
+
+#Random Numbers
+print(random.randrange(1, 10))
+
+#Casting
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+#Strings
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+#Strings as Array
+a = "Hello, World!"
+print(a[1])
+
+#Loop over string
+for x in "banana":
+  print(x)
+  
+a = "Hello, World!"
+print(len(a))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
