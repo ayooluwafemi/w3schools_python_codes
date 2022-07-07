a8c5117898d3d75ae24abd88a271ec77fbcc0ead
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 20:29:40 +0100

    Math Function

--- main.py
+++ main.py
@@ -967,3 +967,15 @@ print(x.strftime("%A"))
 print(x.strftime("%B"))
 print(x.strftime("%d"))
 
+ Math Functions
+x = min(5, 10, 25)
+y = max(5, 10, 25) # can be used to find the lowest or highest value in an iterabl
+x = abs(-7.25) # returns the absolute (positive) value of the specified number
+x = pow(4, 3) # returns the value of x to the power of y (xy).
+x = math.sqrt(64) # returns the square root of a number
+x = math.ceil(1.4) # method rounds a number upwards to its nearest integer
+y = math.floor(1.4) # method rounds a number downwards to its nearest integer
+x = math.pi
+
+print(x)
+print(y)
