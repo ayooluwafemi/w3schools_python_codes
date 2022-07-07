7f95e20256d6440b6b34d08e101cced98991de02
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 20:28:02 +0100

    Inheritance

--- main.py
+++ main.py
@@ -889,5 +889,42 @@ class Person:
 p1 = Person("John", 36)
 p1.myfunc()
 
+# Python Inheritance
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+  def printname(self):
+    print(self.firstname, self.lastname)
+class Student(Person):
+  pass # Use the pass keyword when you do not want to add any other properties or methods to the class.
+x = Student("Mike", "Olsen")
+x.printname()
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+x = Student("Mike", "Olsen", 2019)
+x.welcome()
+
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+  def __next__(self):
+    if self.a <= 8:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
+myclass = MyNumbers()
+myiter = iter(myclass)
+for x in myiter:
+  print(x)
+
 
 
