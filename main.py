7b77f375f4a78c6bc8d0e75caa0be0cc3cfcbcb8
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 16:21:53 +0100

    Object, Class and Methods

--- main.py
+++ main.py
@@ -525,7 +525,6 @@ y = {"google", "microsoft", "apple"}
 z = x.symmetric_difference(y)
 print(z)
 
-
 thisdict = {
   "brand": "Ford",
   "model": "Mustang",
@@ -723,12 +722,184 @@ b = 33
 c = 500
 if a > b and c > a:
   print("Both conditions are True")
-  
+
 # Python While Loops
 i = 1
 while i < 6:
   print(i)
   i += 1
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+  
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+# For loop and Condition
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+#Increment the sequence with 3 (default is 1):
+for x in range(2, 30, 3):
+  print(x)
+
+# Print all numbers from 0 to 5, and print a message when the loop has ended:
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+# Nested Loops: A nested loop is a loop inside a loop.
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+# Creating a Function
+def my_function():
+  print("Hello from a function")
+my_function()
+
+def my_function(fname):
+  print(fname + " Refsnes")
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+my_function("Emil", "Refsnes")
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+my_function("Emil", "Tobias", "Linus")
+
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+my_function(fname = "Tobias", lname = "Refsnes")
+
+def my_function(country = "Norway"):
+  print("I am from " + country)
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+def my_function(food):
+  for x in food:
+    print(x)
+fruits = ["apple", "banana", "cherry"]
+my_function(fruits)
+
+def my_function(x):
+  return 5 * x
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+print("\n\nRecursion Example Results")
+tri_recursion(6)
+
+# Python Lambda: A lambda function is a small anonymous function.
+# Add 10 to argument a, and return the result:
+x = lambda a : a + 10
+print(x(5))
+
+# Multiply argument a with argument b and return the result:
+x = lambda a, b : a * b
+print(x(5, 6))
+
+# Why Use Lambda Functions?
+def myfunc(n):
+  return lambda a : a * n
+mydoubler = myfunc(2)
+print(mydoubler(11))
+
+def myfunc(n):
+  return lambda a : a * n
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+print(mydoubler(11))
+print(mytripler(11))
+
+# Python Classes and Objects
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+p1 = Person("John", 36)
+print(p1.name)
+print(p1.age)
+
+# Object Methods
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+p1 = Person("John", 36)
+p1.myfunc()
+
+# Python Inheritance
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
