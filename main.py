18f3d3996d8920c7e69b9f6b93a400fdf931dffd
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 20:27:38 +0100

    Class and Objects

--- main.py
+++ main.py
@@ -870,6 +870,24 @@ mytripler = myfunc(3)
 print(mydoubler(11))
 print(mytripler(11))
 
+# Python Classes and Objects
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+p1 = Person("John", 36)
+print(p1.name)
+print(p1.age)
+
+# Object Methods
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+p1 = Person("John", 36)
+p1.myfunc()
 
 
 
