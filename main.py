f00910202a2673d5cff6731f9daeafe59e74abfb
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 20:19:39 +0100

    List

--- main.py
+++ main.py
@@ -213,6 +213,165 @@ else:
 x = 200
 print(isinstance(x, int))
 
+# List
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+print(len(thislist))
+print(type(thislist))
+print(thislist[1])
+print(thislist[-1])
+print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+thislist[1] = "blackcurrant" # Change the second item:
+print(thislist)
+
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+thislist.insert(2, "watermelon")
+print(thislist)
+
+thislist.append("orange")
+print(thislist)
+
+# Extend List
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
+
+#Remove from list
+thislist.remove("banana")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+thislist.pop()
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+#Clear the List
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+
+# Loop on List
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+  
+# Loop through Index
+for i in range(len(thislist)):
+  print(thislist[i])
+  
+# Using a while loop
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+# Looping Using List Comprehension
+[print(x) for x in thislist]
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+print(newlist)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if "a" in x]
+print(newlist)
+
+newlist = [x for x in fruits if x != "apple"]
+newlist = [x for x in fruits]
+newlist = [x for x in range(10)]
+newlist = [x for x in range(10) if x < 5]
+newlist = ['hello' for x in fruits]
+newlist = [x if x != "banana" else "orange" for x in fruits]
+
+# Sort List
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+
+thislist.sort(reverse = True)
+print(thislist)
+
+def myfunc(n):
+  return abs(n - 50)
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+
+# Case sensitive sorting can give an unexpected result:
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+mylist = list(thislist)
+print(mylist)
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+list3 = list1 + list2
+print(list3)
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+for x in list2:
+  list1.append(x)
+print(list1)
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+list1.extend(list2)
+print(list1)
+
+#Using get method to retrieve valu from list
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(
+car.get("model")
+)
+
 
 
 
