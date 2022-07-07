65cbe355288f27a91f6ca6a7bf929c2ba26a8fde
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 20:15:41 +0100

    variables are created

--- main.py
+++ main.py
@@ -14,3 +14,42 @@ print("Hello, World!") #This is a comment
 # Python Indentation
 if 5 > 2:
     print("Five is greater than two!")
+
+# variables are created when you assign a value to it
+x = 5
+y = "John"
+print(x)
+print(y)
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
