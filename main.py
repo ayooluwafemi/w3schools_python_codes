1817d32c623490a938dc782d3c35e81e50dec774
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 20:24:13 +0100

    WHILE Loop

--- main.py
+++ main.py
@@ -729,6 +729,32 @@ c = 500
 if a > b and c > a:
   print("Both conditions are True")
 
+# Python While Loops
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+  
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
 
 
 
