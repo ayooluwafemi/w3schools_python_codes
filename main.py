9ea2355f7615ec2328fd26997de1e5908f91e844
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 20:27:18 +0100

    LAMBDA

--- main.py
+++ main.py
@@ -848,6 +848,28 @@ def tri_recursion(k):
 print("\n\nRecursion Example Results")
 tri_recursion(6)
 
+# Python Lambda: A lambda function is a small anonymous function.
+# Add 10 to argument a, and return the result:
+x = lambda a : a + 10
+print(x(5))
+
+# Multiply argument a with argument b and return the result:
+x = lambda a, b : a * b
+print(x(5, 6))
+
+# Why Use Lambda Functions?
+def myfunc(n):
+  return lambda a : a * n
+mydoubler = myfunc(2)
+print(mydoubler(11))
+
+def myfunc(n):
+  return lambda a : a * n
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+print(mydoubler(11))
+print(mytripler(11))
+
 
 
 
